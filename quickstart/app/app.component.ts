0bdcc9fd1e48c1383d06391b45823cfb7eb1ff37
Victor Eliasson <victor.eliasson@webdeziner.se>
2016-12-27 11:57:40 +0100

    Tour of heroes part 1

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent { }
+export class AppComponent  { name = 'Angular'; }
