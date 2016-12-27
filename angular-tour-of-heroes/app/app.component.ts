71ff5bbe84ebbade4ba088decda7b3d8e08fa956
Victor Eliasson <victor.eliasson@webdeziner.se>
2016-12-27 12:35:59 +0100

    Tour of heroes part 3

--- angular-tour-of-heroes/app/app.component.ts
+++ angular-tour-of-heroes/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -13,11 +14,6 @@ const HEROES: Hero[] = [
   { id: 20, name: 'Tornado' }
 ];
 
-export class Hero {
-  id:number;
-  name:string;
-}
-
 @Component({
   selector: 'my-app',
   template:`
@@ -27,14 +23,7 @@ export class Hero {
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles: [`
   .selected {
